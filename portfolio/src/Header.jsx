84e9d6abedc3fe959a1d0691c6322ab470bd7d6a
Jose <victorsantanaa2011@hotmail.com>
2024-01-08 18:10:39 -0300

    Criando o projeto Portfolio

---
+++ portfolio/src/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <img src="" alt="" />
+        <div className="navbar">
+            <a href="#">Sobre mim</a>
+            <a href="#">Tecnologias</a>
+            <a href="#">Projetos</a>
+            <a href="#">Linktree</a>
+        </div>
+    </div>
+  )
+}
+
+export default Header
