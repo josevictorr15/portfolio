699af474909d77948744b85305801c329dc28a74
Jose <victorsantanaa2011@hotmail.com>
2024-01-10 08:41:52 -0300

    Configurando o header

--- portfolio/src/Header.jsx
+++ portfolio/src/Header.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import "./Header.css"
+import logo from "./assets/Lovepik_com-450044973-Vector Programming Icon.png"
+import phone from "./assets/telefone-tocando.png"
 
 const Header = () => {
   return (
     <div className='header'>
-        <img src="" alt="" />
+        <img src={logo} alt='Logo do portfolio' />
         <div className="navbar">
-            <a href="#">Sobre mim</a>
-            <a href="#">Tecnologias</a>
-            <a href="#">Projetos</a>
-            <a href="#">Linktree</a>
+          <a href="#">Sobre mim</a>
+          <a href="#">Projetos</a>
+          <a href="#">Habilidades</a>
+          <a href="#">Linktree</a>
+        </div>
+        <div className="language-change">
+          <p>Idioma</p>
         </div>
     </div>
   )
